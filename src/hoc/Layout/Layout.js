519f6bc5c0474d2f78f645f68ef5cecab8d4e364
PesheVik <peshevik@mail.ru>
2021-07-07 23:59:53 +0300

    #4 - use css classes

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import classes from './Layout.css'
 
 class Layout extends Component {
   render () {
     return (
       /*корневой див всего проекта, на который будет повешены глобальные стили + сюда будут выноситься корневые меню*/
-      <div>
+      <div className={classes.Layout}>
         {/*весь контент будет тут (все страницы приложения)*/}
         <main>
           {this.props.children}
