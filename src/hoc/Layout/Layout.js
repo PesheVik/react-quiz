e68dc983090e1c3e730eecd9c563989042df3e69
PesheVik <peshevik@mail.ru>
2021-07-08 20:20:02 +0300

    #56 - feature, кнопка переключения меню

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import classes from './Layout.css'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
 
 class Layout extends Component {
+
+  state = {
+    menu: false,
+  }
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu,
+    })
+  }
+
   render () {
     return (
       /*корневой див всего проекта, на который будет повешены глобальные стили + сюда будут выноситься корневые меню*/
       <div className={classes.Layout}>
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
+
         {/*весь контент будет тут (все страницы приложения)*/}
         <main>
           {this.props.children}
